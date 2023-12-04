af3d607be91d287e14b797ba6ca93a60de50ee8e
iuryr <iuryroberto@gmail.com>
2023-12-04 15:34:34 -0300

    (feat): lexer typedef, set_lexer & read_char functions)

--- includes/minishell.h
+++ includes/minishell.h
@@ -15,6 +15,16 @@ typedef struct s_tk
 {
 	t_tk_type	type;
 	char		*literal;
+	struct s_tk	*next;
 } t_tk;
+
+typedef struct s_lexer
+{
+	char	*input;
+	unsigned int	pos;
+	unsigned int	read_pos;
+	char			ch;
+	unsigned int	size;
+} t_lexer;
 
 #endif //MINISHELL_H
--- minishell.c
+++ minishell.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 #include "includes/minishell.h"
 
+void	set_lexer(t_lexer *lexer, char *input)
+{
+	lexer->input = input;
+	lexer->pos = 0;
+	lexer->read_pos = 0;
+	lexer->ch = 0;
+	lexer->size = ft_strlen(input);
+}
+
+void	read_char(t_lexer *l)
+{
+	if (l->read_pos >= l->size)
+		l->ch = 0;
+	else
+		l->ch = l->input[l->read_pos];
+	l->pos = l->read_pos;
+	l->read_pos++;
+}
+
 int main(void)
 {
 	char *input;
+	t_lexer	lexer;
 
-	while (1)
-	{
-		input = readline("$>");
-		printf("%s\n", input);
-		free(input);
-	}
+	input = readline("$>");
+	set_lexer(&lexer, input);
+	free(input);
 
 	return (0);
 }
