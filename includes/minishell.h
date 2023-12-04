18a948118a16ce37a44401b1089da0c58218d658
iury <iuryroberto@gmail.com>
2023-12-04 12:57:36 -0300

    (other) continue from 42

--- includes/minishell.h
+++ includes/minishell.h
@@ -2,5 +2,18 @@
 # define MINISHELL_H
 
 #include "../libft/libft.h"
+#include <readline/readline.h>
+
+typedef struct s_word
+{
+	char *word;
+	int	flags;
+} t_word;
+
+typedef struct s_word_list
+{
+	t_word word;
+	t_word *next;
+} t_word_list;
 
 #endif //MINISHELL_H
--- minishell.c
+++ minishell.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+#include "includes/minishell.h"
+
 int main(void)
 {
+	char *input;
+
+	while (1)
+	{
+		input = readline("$>");
+		printf("%s\n", input);
+		free(input);
+	}
+
 	return (0);
 }
