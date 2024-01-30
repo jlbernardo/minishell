ca5009c589f14384c4d0effd86e06fa7b8ffab88
Juliany <julianybernardo@outlook.com>
2024-01-30 16:28:05 -0300

    (feat) Finisher function that frees tokens and AST

--- src/finisher.c
+++ src/finisher.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   finisher.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Juliany Bernardo <julberna@student.42sp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 17:38:30 by julberna          #+#    #+#             */
-/*   Updated: 2024/01/30 16:23:33 by Juliany Ber      ###   ########.fr       */
+/*   Created: 2024/01/29 20:03:39 by Juliany Ber       #+#    #+#             */
+/*   Updated: 2024/01/30 15:48:49 by Juliany Ber      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-int	main(void)
+void	finisher(t_token *tokens, t_ast_node *ast)
 {
-	int			control;
-	t_token		*tokens;
-	t_ast_node	*ast;
-
-	control = 42;
-	while (control)
-	{
-		if (lexer(&tokens, &ast))
-			parser(tokens, &ast);
-		finisher(tokens, ast);
-	}
-	return (0);
+	free_ast(ast);
+	free_tokens(tokens);
 }
--- src/moses.c
+++ src/moses.c
@@ -6,11 +6,65 @@
 /*   By: Juliany Bernardo <julberna@student.42sp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 19:49:32 by julberna          #+#    #+#             */
-/*   Updated: 2024/01/29 19:22:54 by Juliany Ber      ###   ########.fr       */
+/*   Updated: 2024/01/29 20:03:15 by Juliany Ber      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
+
+void	free_wl2(t_wl_element **wl)
+{
+	if (wl == NULL || *wl == NULL)
+		return ;
+	free_wl2(&(*wl)->next);
+	free((*wl)->word);
+	free(*wl);
+}
+
+void	free_redirects2(t_redirect **rl)
+{
+	if (rl == NULL || *rl == NULL)
+		return ;
+	free_redirects2(&(*rl)->next);
+	free((*rl)->filename);
+	free(*rl);
+}
+
+void	free_data(t_cmd	*cmd)
+{
+	if (cmd == NULL)
+		return ;
+	free(cmd->pathname);
+	free_wl2(cmd->word_list);
+	free(cmd->word_list);
+	free_redirects2(cmd->redirects);
+	free(cmd->redirects);
+	free(cmd);
+}
+
+void	free_tokens(t_token *tokens)
+{
+	t_token	*temp;
+
+	while (tokens)
+	{
+		temp = tokens;
+		tokens = tokens->next;
+		free(temp->literal);
+		free(temp);
+	}
+}
+
+void	free_ast(t_ast_node *ast)
+{
+	if (ast == NULL)
+		return ;
+	free_ast(ast->right);
+	free_ast(ast->left);
+	if (ast->type == CMD)
+		free_data(ast->data);
+	free(ast);
+}
 
 // void	finisher(t_token **tokens, t_ast_node **ast)
 // {
@@ -35,15 +89,6 @@
 // 	free(*wl);
 // }
 
-void	free_wl2(t_wl_element **wl)
-{
-	if (wl == NULL || *wl == NULL)
-		return ;
-	free_wl2(&(*wl)->next);
-	free((*wl)->word);
-	free(*wl);
-}
-
 //for recursive implementation see free_redirects2 below
 // void	free_redirects(t_redirect **rl)
 // {
@@ -61,35 +106,3 @@ void	free_wl2(t_wl_element **wl)
 // 	free((*rl)->filename);
 // 	free(*rl);
 // }
-
-void	free_redirects2(t_redirect **rl)
-{
-	if (rl == NULL || *rl == NULL)
-		return ;
-	free_redirects2(&(*rl)->next);
-	free((*rl)->filename);
-	free(*rl);
-}
-
-void	free_data(t_cmd	*cmd)
-{
-	if (cmd == NULL)
-		return ;
-	free(cmd->pathname);
-	free_wl2(cmd->word_list);
-	free(cmd->word_list);
-	free_redirects2(cmd->redirects);
-	free(cmd->redirects);
-	free(cmd);
-}
-
-void	free_ast(t_ast_node *ast)
-{
-	if (ast == NULL)
-		return ;
-	free_ast(ast->right);
-	free_ast(ast->left);
-	if (ast->type == CMD)
-		free_data(ast->data);
-	free(ast);
-}
