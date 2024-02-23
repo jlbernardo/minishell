740e66810667267dba7c7ad01bbdd2d46e818eb3
Juliany <julianybernardo@outlook.com>
2024-02-23 15:56:38 -0300

    (fix) Segfault on exit as first command

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: Juliany Bernardo <julberna@student.42sp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 17:38:30 by julberna          #+#    #+#             */
-/*   Updated: 2024/02/22 21:21:06 by Juliany Ber      ###   ########.fr       */
+/*   Updated: 2024/02/23 15:55:32 by Juliany Ber      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	set_meta(t_meta *meta, char **__environ)
 {
 	meta->hash = ft_calloc(HT_SIZE, sizeof(t_hash *));
 	add_env_to_ht(__environ, meta->hash);
+	add_or_upd_ht_entry("?", "0", meta->hash);
 }
 
 int	main(void)
