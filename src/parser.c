d8b4d506971fda1ce20c57925a64e480fadb50d1
Juliany <julianybernardo@outlook.com>
2024-02-24 23:40:57 -0300

    (chore) Remove split_tokens function

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: Juliany Bernardo <julberna@student.42sp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 21:12:03 by julberna          #+#    #+#             */
-/*   Updated: 2024/02/24 22:19:09 by Juliany Ber      ###   ########.fr       */
+/*   Updated: 2024/02/24 23:38:04 by Juliany Ber      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,29 +58,6 @@ void	remove_quotes(t_token **tokens)
 	remove_quotes(&(*tokens)->next);
 }
 
-void	split_tokens(t_meta *meta)
-{
-	char	*new_str;
-	t_token	*tk_next;
-	t_token	*tk;
-
-	tk = meta->tokens;
-	while (tk)
-	{
-		if (check_split(tk->literal))
-		{
-			update_literal(tk, &new_str);
-			tk_next = tk->next;
-			tk->next = NULL;
-			new_token(&tk, WORD, new_str);
-			tk_last(tk)->next = tk_next;
-			free(new_str);
-			split_tokens(meta);
-		}
-		tk = tk->next;
-	}
-}
-
 void	remove_empty_tokens(t_token **tokens)
 {
 	t_token	*curr;
@@ -88,7 +65,7 @@ void	remove_empty_tokens(t_token **tokens)
 	curr = *tokens;
 	if (!tokens || !*tokens)
 		return ;
-	if (!curr->literal || ft_strlen(curr->literal) == 0)
+	if (!curr->literal)
 	{
 		*tokens = curr->next;
 		free(curr->literal);
