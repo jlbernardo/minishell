807673a5d12a6f21abe55d7c6913cc0f542cce2f
Juliany <julianybernardo@outlook.com>
2024-03-02 20:21:19 -0300

    (chore) Fix capitalization on redirect error messages Also placed these error messages on error_handler.c

--- src/utils/error_handler.c
+++ src/utils/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: Juliany Bernardo <julberna@student.42sp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:25:16 by Juliany Ber       #+#    #+#             */
-/*   Updated: 2024/02/28 14:48:33 by Juliany Ber      ###   ########.fr       */
+/*   Updated: 2024/03/02 20:11:31 by Juliany Ber      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,18 @@ void	path_error(t_meta *meta, char *path, char *msg, int exit_code)
 	ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putendl_fd(msg, STDERR_FILENO);
 	finisher(*meta, "ATHE", exit_code);
+}
+
+void	print_nsf_error_msg(char *filename)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(filename, 2);
+	ft_putstr_fd(": No such file or directory\n", 2);
+}
+
+void	print_np_error_msg(char *filename)
+{
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(filename, 2);
+	ft_putstr_fd(": Permission denied\n", 2);
 }
